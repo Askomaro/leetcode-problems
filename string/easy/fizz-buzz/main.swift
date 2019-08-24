e92af0dac448a24aa7d792af2839ffb0598728f4
anton_skomarovskyi <anton.skomarovskyi@gmail.com>
2019-08-24 10:51:17 +0300

    Fix solution

--- string/easy/fizz-buzz/main.swift
+++ string/easy/fizz-buzz/main.swift
@@ -30,21 +30,24 @@ Return:
 
 class Solution {
     func fizzBuzz(_ n: Int) -> [String] {
-        var res : String = []
+        var res : [String] = []
         let fizz = "Fizz"
         let buzz = "Buzz"
 
-        for i in n{
+        for i in 1...n{
             if i % 3 == 0 && i % 5 == 0 {
                 res.append(fizz+buzz)
                 continue
             }
             if i % 3 == 0 {
                 res.append(fizz)
+                continue
             }
             if i % 5 == 0 {
                 res.append(buzz)
+                continue
             }
+            res.append(String(i))
         }
 
         return res
