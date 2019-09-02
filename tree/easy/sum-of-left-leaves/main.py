feadd9a18817303ae454137a309de9ea9c801a3d
anton_skomarovskyi <anton.skomarovskyi@gmail.com>
2019-09-02 13:03:08 +0300

    Add recursive solution

--- tree/easy/sum-of-left-leaves/main.py
+++ tree/easy/sum-of-left-leaves/main.py
@@ -18,8 +18,28 @@
 #         self.right = None
 
 class Solution(object):
+    sum_leafs = 0
+
     def sumOfLeftLeaves(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
+        if not root:
+            return self.sum_leafs
+
+        self.isLeftLeaf(root.left, True)
+        self.isLeftLeaf(root.right, False)
+
+        return self.sum_leafs
+
+    def isLeftLeaf(self, root, is_left):
+        if not root:
+            return
+
+        if not root.left and not root.right and is_left:
+            self.sum_leafs += root.val
+            return
+
+        self.isLeftLeaf(root.left, True)
+        self.isLeftLeaf(root.right, False)
